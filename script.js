20949ba80ad9babefacab14ebc187dc301453662
MOUNAIM <105808794+moun3ym@users.noreply.github.com>
2023-01-15 21:47:34 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function updateTime() {
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}
+
+setInterval(updateTime, 1000);
